d18e4e44e544a8a2c81f7102c8a28295b61b3785
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2023-07-21 06:38:13 +0800

    publishing events to turn lights on/off

--- mobile/lights_up/lib/views/mission_control/mission_control_view.dart
+++ mobile/lights_up/lib/views/mission_control/mission_control_view.dart
@@ -1,12 +1,20 @@
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter/material.dart';
 
 import '../../constants/app.dart';
+import '../../constants/mqtt_topics.dart';
+import '../../state/providers/mqtt_client_manager_provider.dart';
 
-class MissionControlView extends StatelessWidget {
+class MissionControlView extends HookConsumerWidget {
   const MissionControlView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final mqttManager = ref.watch(mqttClientManagerProvider);
+
+    final isLightsOn = useState(false);
+
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
@@ -18,15 +26,22 @@ class MissionControlView extends StatelessWidget {
                 style: Theme.of(context).textTheme.titleMedium,
               ),
               Switch(
-                value: true,
-                onChanged: (value) {},
+                value: isLightsOn.value,
+                onChanged: (value) {
+                  isLightsOn.value = value;
+                  mqttManager?.publish(
+                    kBedRoomLights,
+                    isLightsOn.value ? '1' : '0',
+                  );
+                },
               ),
             ],
           ),
           Icon(
             Icons.lightbulb_circle_rounded,
             size: 350,
-            color: Colors.grey.shade300,
+            color:
+                isLightsOn.value ? Colors.amber.shade300 : Colors.grey.shade300,
           ),
         ],
       ),
